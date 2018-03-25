1f7c795bc187d010cfc00a60ac172860d4d09df7
藤崎 友則 <naoki@MacBook-Air.local>
2018-03-25 15:19:07 +0900

    ボードの数字を表示、コメント追加

--- script/darts.js
+++ script/darts.js
@@ -20,6 +20,9 @@ var COLOR_ARRANGE_EVEN = "yellow";
 var COLOR_ARRANGE_ODD = "lightpink";
 var COLOR_NONE = "white";
 
+/**
+ * 初期処理
+ */
 $( function() {
 	// ボード描画
 	canvas_init();
@@ -29,7 +32,7 @@ $( function() {
 	var param = getUrlVars();
 	$("#point").text(param.point);
 
-        for (var i = 1; i <= 20; i++) {
+    for (var i = 1; i <= 20; i++) {
 		displayPoint(param.point, i);
 	}
 	displayPointForBull(param.point, 25);
@@ -37,6 +40,123 @@ $( function() {
 	$('canvas').drawLayers();
 });
 
+/**
+ * アレンジ表作成
+ */
+function createArrangeTable() {
+	var num = "";
+	var tagTD = "";
+	var tagTR = "";
+	for (var i = 1; i<=10; i++) {
+		num = NUMBER_MAP[i];
+		tagTD = '<th>' + num + '</th>';
+		tagTD = tagTD + '<td id="' + num + 'S"></td>';
+		tagTD = tagTD + '<td id="' + num + 'D"></td>';
+		tagTD = tagTD + '<td id="' + num + 'T"></td>';
+		
+		num = NUMBER_MAP[i + 10];
+		tagTD = tagTD + '<th>' + num + '</th>';
+		tagTD = tagTD + '<td id="' + num + 'S"></td>';
+		tagTD = tagTD + '<td id="' + num + 'D"></td>';
+		tagTD = tagTD + '<td id="' + num + 'T"></td>';
+
+		tagTR = '<tr>'+tagTD+'</tr>';
+		$("#arrange_table").append(tagTR);
+	}
+	num = NUMBER_MAP[i];
+	tagTD = '<th>ー</th>';
+	tagTD = tagTD + '<td>ー</td>';
+	tagTD = tagTD + '<td>ー</td>';
+	tagTD = tagTD + '<td>ー</td>';
+		
+	num = NUMBER_MAP[i + 10];
+	tagTD = tagTD + '<th>BULL</th>';
+	tagTD = tagTD + '<td id="SB"></td>';
+	tagTD = tagTD + '<td id="DB"></td>';
+	tagTD = tagTD + '<td>ー</td>';
+	tagTR = '<tr>'+tagTD+'</tr>';
+	$("#arrange_table").append(tagTR);	
+}
+
+/**
+ * カラー取得
+ * @param {取得点数} hit_zanpoint 
+ */
+function getColor(hit_zanpoint) {
+	if (hit_zanpoint == BURST) {
+		return COLOR_BURST;
+	}
+	if (hit_zanpoint == OUT){
+		return COLOR_OUT;
+	}
+	if (hit_zanpoint != ""){
+		if (isEvenNumber(hit_zanpoint)) {
+			// 偶数
+			return COLOR_ARRANGE_EVEN;
+		} else {
+			// 奇数
+			return COLOR_ARRANGE_ODD;
+		}
+	}
+	return COLOR_NONE;
+}
+
+/**
+ * 表示点数文字列取得
+ * @param {残点数} zanPoint 
+ * @param {HitNumber} hitNumber 
+ */
+function displayPoint(zanPoint, hitNumber) {
+	// シングルヒット時の計算
+	var hit_zanpoint = calc(zanPoint, hitNumber * 1, HIT_AREA_SINGLE);
+	var color = getColor(hit_zanpoint);
+	$("#" + hitNumber + "S").text(getDisplayText(hitNumber + "S", hit_zanpoint));
+	$("#" + hitNumber + "S").css({backgroundColor: color});
+	$('canvas').getLayer(hitNumber + "IS").fillStyle = color;
+	$('canvas').getLayer(hitNumber + "OS").fillStyle = color;
+
+	// ダブルヒット時の計算
+	hit_zanpoint = calc(zanPoint, hitNumber * 2, HIT_AREA_DOUBLE);
+	color = getColor(hit_zanpoint);
+	$("#" + hitNumber + "D").text(getDisplayText(hitNumber + "D", hit_zanpoint));
+	$("#" + hitNumber + "D").css({backgroundColor: color});
+	$('canvas').getLayer(hitNumber + "D").fillStyle = color;
+
+	// トリプル計算時の計算
+	hit_zanpoint = calc(zanPoint, hitNumber * 3, HIT_AREA_TRIPLE);
+	color = getColor(hit_zanpoint);
+	$("#" + hitNumber + "T").text(getDisplayText(hitNumber + "T", hit_zanpoint));
+	$("#" + hitNumber + "T").css({backgroundColor: color});
+	$('canvas').getLayer(hitNumber + "T").fillStyle = color;
+}
+
+/**
+ * 表示点数文字列取得(bull)
+ * @param {残点数} zanPoint 
+ * @param {*} hitNumber 
+ */
+function displayPointForBull(zanPoint, hitNumber) {
+	// シングルブル時の計算
+	var hit_zanpoint = calc(zanPoint, hitNumber * 1, HIT_AREA_SINGLE);
+	var color = getColor(hit_zanpoint);
+	$("#SB").text(getDisplayText("SB", hit_zanpoint));
+	$("#SB").css({backgroundColor: color});
+	$('canvas').getLayer("SB").fillStyle = getColor(hit_zanpoint);
+
+	// ダブルブルヒット時の計算
+	hit_zanpoint = calc(zanPoint, hitNumber * 2, HIT_AREA_DOUBLE);
+	color = getColor(hit_zanpoint);
+	$("#DB").text(getDisplayText("DB", hit_zanpoint));
+	$("#DB").css({backgroundColor: color});
+	$('canvas').getLayer("DB").fillStyle = getColor(hit_zanpoint);
+}
+
+/**
+ * 計算処理
+ * @param {残点数} zanPoint 
+ * @param {取得点数} hitPoint 
+ * @param {シングル/ダブル/トリプル} hitArea 
+ */
 function calc(zanPoint, hitPoint, hitArea) {
 	var zan = zanPoint - hitPoint;
 	var masterOutFlag = false;
@@ -104,98 +224,6 @@ function calc(zanPoint, hitPoint, hitArea) {
 		}
 	}
 	return "";
-}
-
-/**
- * アレンジ表作成
- */
-function createArrangeTable() {
-	var num = "";
-	var tagTD = "";
-	var tagTR = "";
-	for (var i = 1; i<=10; i++) {
-		num = NUMBER_MAP[i];
-		tagTD = '<th>' + num + '</th>';
-		tagTD = tagTD + '<td id="' + num + 'S"></td>';
-		tagTD = tagTD + '<td id="' + num + 'D"></td>';
-		tagTD = tagTD + '<td id="' + num + 'T"></td>';
-		
-		num = NUMBER_MAP[i + 10];
-		tagTD = tagTD + '<th>' + num + '</th>';
-		tagTD = tagTD + '<td id="' + num + 'S"></td>';
-		tagTD = tagTD + '<td id="' + num + 'D"></td>';
-		tagTD = tagTD + '<td id="' + num + 'T"></td>';
-
-		tagTR = '<tr>'+tagTD+'</tr>';
-		$("#arrange_table").append(tagTR);
-	}
-	num = NUMBER_MAP[i];
-	tagTD = '<th>ー</th>';
-	tagTD = tagTD + '<td>ー</td>';
-	tagTD = tagTD + '<td>ー</td>';
-	tagTD = tagTD + '<td>ー</td>';
-		
-	num = NUMBER_MAP[i + 10];
-	tagTD = tagTD + '<th>BULL</th>';
-	tagTD = tagTD + '<td id="SB"></td>';
-	tagTD = tagTD + '<td id="DB"></td>';
-	tagTD = tagTD + '<td>ー</td>';
-	tagTR = '<tr>'+tagTD+'</tr>';
-	$("#arrange_table").append(tagTR);	
-}
-
-function getColor(hit_zanpoint) {
-	if (hit_zanpoint == BURST) {
-		return COLOR_BURST;
-	}
-	if (hit_zanpoint == OUT){
-		return COLOR_OUT;
-	}
-	if (hit_zanpoint != ""){
-		if (isEvenNumber(hit_zanpoint)) {
-			// 偶数
-			return COLOR_ARRANGE_EVEN;
-		} else {
-			// 奇数
-			return COLOR_ARRANGE_ODD;
-		}
-	}
-	return COLOR_NONE;
-}
-
-function displayPoint(point, hitNumber) {
-	var hit_zanpoint = calc(point, hitNumber * 1, HIT_AREA_SINGLE);
-	var color = getColor(hit_zanpoint);
-	$("#" + hitNumber + "S").text(getDisplayText(hitNumber + "S", hit_zanpoint));
-	$("#" + hitNumber + "S").css({backgroundColor: color});
-	$('canvas').getLayer(hitNumber + "IS").fillStyle = color;
-	$('canvas').getLayer(hitNumber + "OS").fillStyle = color;
-
-	hit_zanpoint = calc(point, hitNumber * 2, HIT_AREA_DOUBLE);
-	color = getColor(hit_zanpoint);
-	$("#" + hitNumber + "D").text(getDisplayText(hitNumber + "D", hit_zanpoint));
-	$("#" + hitNumber + "D").css({backgroundColor: color});
-	$('canvas').getLayer(hitNumber + "D").fillStyle = color;
-
-	hit_zanpoint = calc(point, hitNumber * 3, HIT_AREA_TRIPLE);
-	color = getColor(hit_zanpoint);
-	$("#" + hitNumber + "T").text(getDisplayText(hitNumber + "T", hit_zanpoint));
-	$("#" + hitNumber + "T").css({backgroundColor: color});
-	$('canvas').getLayer(hitNumber + "T").fillStyle = color;
-}
-
-function displayPointForBull(point, hitNumber) {
-	var hit_zanpoint = calc(point, hitNumber * 1, HIT_AREA_SINGLE);
-	var color = getColor(hit_zanpoint);
-	$("#SB").text(getDisplayText("SB", hit_zanpoint));
-	$("#SB").css({backgroundColor: color});
-	$('canvas').getLayer("SB").fillStyle = getColor(hit_zanpoint);
-
-	hit_zanpoint = calc(point, hitNumber * 2, HIT_AREA_DOUBLE);
-	color = getColor(hit_zanpoint);
-	$("#DB").text(getDisplayText("DB", hit_zanpoint));
-	$("#DB").css({backgroundColor: color});
-	$('canvas').getLayer("DB").fillStyle = getColor(hit_zanpoint);
 }
 
 function getDisplayText(hitNumber, hit_zanpoint) {
